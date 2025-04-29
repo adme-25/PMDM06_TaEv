56dab541b5e89d255d0cf319a99db5d51f8446ea
anaDavila <adavila@birt.eus>
2025-04-29 17:49:25 +0200

    primero

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PMDM06_TaEv',
+  webDir: 'www'
+};
+
+export default config;
